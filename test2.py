19ff7bf08362ce940ed162b0bc3be7bf4c0bb66a
lil-rick3 <83485898+lil-rick3@users.noreply.github.com>
2024-01-18 13:30:12 -0700

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,21 @@
+import time
+from rainbowio import colorwheel
+from adafruit_extended_bus import ExtendedI2C as I2C
+from adafruit_seesaw import seesaw, neopixel
+
+i2c = I2C(1, frequency=800000)
+ss = seesaw.Seesaw(i2c, addr=0x60)
+neo_pin = 15
+num_pixels = 30
+
+pixels = neopixel.NeoPixel(ss, neo_pin, num_pixels, brightness = 0.3, auto_write=False)
+
+color_offset = 0
+
+while True:
+    for i in range(num_pixels):
+        rc_index = (i * 256 // num_pixels) + color_offset
+        pixels[i] = colorwheel(rc_index & 255)
+    pixels.show()
+    color_offset += 8
+    time.sleep(0.01)
